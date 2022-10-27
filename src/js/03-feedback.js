7f5b56d3b51183b8350d9ea3e03b77740e942dfc
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-10-27 21:58:53 +0300

    Task №3 is completed.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+};
+const formDate = {};
+    
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormFieldsInput, 500));
+
+populateFormFields();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.target.reset();
+    
+    localStorage.removeItem('feedback-form-state');
+}
+
+function onFormFieldsInput(e) {
+    formDate[e.target.name] = e.target.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formDate))
+}
+
+function populateFormFields() {
+    const formFieldsDate = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (formFieldsDate) {
+        refs.email.value = formFieldsDate.email;
+        refs.message.value = formFieldsDate.message;
+    }
+}
