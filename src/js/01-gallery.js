ae905886afd9e297483a31f40d2533a0e7b7de18
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-10-27 14:57:26 +0300

    Task №1 is completed.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryParentEl = document.querySelector('.gallery');
+
+const galleryEls = galleryItems.map(makeGalleryItem).join('');
+galleryParentEl.insertAdjacentHTML('beforeend', galleryEls);
+galleryParentEl.addEventListener('click', onPreviewImgClick);
+
+const modal = new SimpleLightbox(
+                                    '.gallery a',
+                                    {
+                                        captionsData: 'alt',
+                                        captionDelay: 250,
+                                    }
+                                );
+
+function makeGalleryItem({ preview, original, description }) {
+    const itemEl = `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+                    </a>`;
+    return itemEl;
+}
+
+function onPreviewImgClick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }; 
+
+    modal.open(event);
+}
