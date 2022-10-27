568f5acd593f4ad7fb8b42a5bbe3c0995c68a9fb
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-10-27 19:44:36 +0300

    Task №2 is completed.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+window.addEventListener('load', setCurrentTime);
+
+function getCurrentTime() {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+    })
+}
+
+function setCurrentTime() {
+    player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')));
+} 
